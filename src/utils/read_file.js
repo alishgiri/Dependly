f3807925c904a7541246034cfa096b8ed536eec4
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2021-05-29 19:52:34 +0545

    Rename readFile file to read_file

---
+++ src/utils/read_file.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const path = require("path");
+
+const { dependsOnString } = require("./constants");
+
+module.exports.readFiles = (filename) => {
+  if (!filename) throw Error("No Filename provided!!");
+  return new Promise((resolve, reject) => {
+    const filePath = path.join(__dirname, "..", `/input_files/${filename}`);
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err || !data.includes(dependsOnString)) {
+        reject(err);
+        return;
+      }
+      resolve(data);
+    });
+  });
+};
