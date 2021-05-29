6c132d1075f4d5b06614ed2afb1c23661ea5d66b
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2021-05-29 19:54:10 +0545

    Correct readFile func name

--- src/utils/read_file.js
+++ src/utils/read_file.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const { dependsOnString } = require("./constants");
 
-module.exports.readFiles = (filename) => {
+module.exports.readFile = (filename) => {
   if (!filename) throw Error("No Filename provided!!");
   return new Promise((resolve, reject) => {
     const filePath = path.join(__dirname, "..", `/input_files/${filename}`);
