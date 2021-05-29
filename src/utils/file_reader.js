e2313e801f5f9873005ea1dd93274559eb5ac768
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2021-05-29 20:19:31 +0545

    Update file file_reader
    - Rename file name.
    - Add getAllFiles utiltiy function.

--- src/utils/file_reader.js
+++ src/utils/file_reader.js
@@ -3,10 +3,24 @@ const path = require("path");
 
 const { dependsOnString } = require("./constants");
 
+const rootFolderPath = path.join(__dirname, "..", "/input_files/");
+
+module.exports.getAllFiles = () => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(rootFolderPath, (err, files) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(files);
+    });
+  });
+};
+
 module.exports.readFile = (filename) => {
   if (!filename) throw Error("No Filename provided!!");
   return new Promise((resolve, reject) => {
-    const filePath = path.join(__dirname, "..", `/input_files/${filename}`);
+    const filePath = path.join(rootFolderPath, filename);
     fs.readFile(filePath, "utf8", (err, data) => {
       if (err || !data.includes(dependsOnString)) {
         reject(err);
